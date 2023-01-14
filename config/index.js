659465bd988025904b5fa9bb02491c12af0204ca
Max Anderson MOUMOUNI <happydev.229@gmail.com>
2023-01-14 17:03:31 +0100

    Add config middlewares

--- config/index.js
+++ config/index.js
@@ -1,7 +1,16 @@
 require("dotenv").config();
 
+function ensureConfigInitialized(req, res, next) {
+  if (!config.token_path) {
+    return res.status(500).json("Token path not set");
+  }
+  next();
+}
+
 const config = {
-    port: process.env.PORT || 3000,
+  port: process.env.PORT || 3000,
+  token_path: process.env.TOKEN_PATH,
+  middlewares: [ensureConfigInitialized],
 };
 
 module.exports = config;
--- index.js
+++ index.js
@@ -20,51 +20,62 @@ app.listen(config.port, () => {
 
 //set headers
 function setHeaders(req, res, next) {
-  req.headers.authorization = req.headers.authorization || req.query.token;
+  req.headers.authorization =
+    req.headers.authorization ||
+    res.getHeader("authorization") ||
+    req.query.token;
   next();
 }
 
 //auth middleware
 function auth(req, res, next) {
-  fs.readFile("./auth/token.json", (err, data) => {
+  fs.readFile(config.token_path, (err, data) => {
     if (err) {
       return res.status(500).json("Server error");
     }
     const token = JSON.parse(data);
     if (!token.includes(req.headers.authorization)) {
-      console.log(req.headers.authorization);
       return res.status(401).json("Unauthorized");
     }
-
     next();
   });
 }
 
-app.get("/", [setHeaders, auth], (req, res) => {
-  res.json("Hello World!");
-});
 
-app.get("/get-token", (req, res) => {
-    const token = crypto.randomBytes(16).toString("hex");
-    storeToken(token, res);
-});
-function storeToken(token, res) {
-    fs.readFile("./auth/token.json", (err, data) => {
+function output(req, res) {
+  res.json("Hello World!");
+}
+function create_token(req, res, next) {
+  const token = crypto.randomBytes(16).toString("hex");
+
+  fs.readFile(config.token_path, (err, data) => {
+    if (err) {
+      return res.status(500).json("Cannot read file");
+    }
+    let tokens;
+    try {
+      tokens = JSON.parse(data);
+    } catch (e) {
+      tokens = [];
+    }
+    if (tokens.includes(token)) {
+      return res.status(500).json("Token already exists");
+    }
+    tokens.push(token);
+    fs.writeFile(config.token_path, JSON.stringify(tokens), (err) => {
       if (err) {
-        return res.status(500).json("Server error");
+        return res.status(500).json("Cannot write file");
       }
-      const tokens = JSON.parse(data);
-      if (tokens.includes(token)) {
-        return res.status(500).json("Server error");
-      }
-      tokens.push(token);
-      fs.writeFile("./auth/token.json", JSON.stringify(tokens), (err) => {
-        if (err) {
-          return res.status(500).json("Server error");
-        }
-        res.redirect(`/?token=${token}`);
-      });
+      //redirect to / with the token in headers
+      res.setHeader("authorization", token);
+      next();
     });
-    
+  });
 }
 
+
+const middlewares = [setHeaders, auth, output];
+
+app.get("/", middlewares);
+
+app.get("/get-token", config.middlewares, create_token, middlewares);
